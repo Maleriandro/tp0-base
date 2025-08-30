1910ca21e79f8ce0a9586cbcb0828ed20f6f1170
Mariano Lazzarini <marianolazzarini01@gmail.com>
2025-08-29 22:15:12 -0300

    agregado script validacion

---
+++ validar-echo-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SERVER_IP="server"
+SERVER_PORT="12345"
+NETWORK_NAME="tp0_testing_net"
+
+
+MESSAGE="mensaje_de_prueba_tp0"
+
+RESULT=$(docker run --rm --network $NETWORK_NAME busybox sh -c "echo '$MESSAGE' | nc $SERVER_IP $SERVER_PORT" 2>/dev/null)
+
+if [ "$RESULT" = "$MESSAGE" ]; then
+  echo "action: test_echo_server | result: success"
+else
+  echo "action: test_echo_server | result: fail"
+fi
