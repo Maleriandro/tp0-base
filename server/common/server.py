365362c0e223763072a0bc1d977c74e52b6c8ce7
Mariano Lazzarini <marianolazzarini01@gmail.com>
2025-08-30 18:50:06 -0300

    cerrar socket conectado a cliente en server

--- server/common/server.py
+++ server/common/server.py
@@ -9,17 +9,29 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._current_client_socket = None
         self._stopped = False
         
         signal.signal(signal.SIGTERM, self.__stop_server)
 
     def __stop_server(self, signum, frame):
+        # Parar loop
         logging.info("action: stop_server | result: in_progress")
         self._stopped = True
+
+        # Cerrar socket servidor
         logging.debug("action: stop_server_socket | result: in_progress")
         if self._server_socket:
             self._server_socket.close()
+        self._server_socket = None
         logging.debug("action: stop_server_socket | result: success")
+
+        # Cerrar socket cliente
+        logging.debug("action: stop_client_socket | result: in_progress")
+        if self._current_client_socket:
+            self._current_client_socket.close()
+        self._current_client_socket = None
+        logging.debug("action: stop_client_socket | result: success")
 
     def run(self):
         """
@@ -33,14 +45,14 @@ class Server:
 
         while not self._stopped:
             try:
-                client_sock = self.__accept_new_connection()
-                self.__handle_client_connection(client_sock)
+                self._current_client_socket = self.__accept_new_connection()
+                self.__handle_client_connection()
             except OSError:
                 break
             
         logging.info("action: stop_server | result: success")
 
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(self):
         """
         Read message from a specific client socket and closes the socket
 
@@ -49,15 +61,17 @@ class Server:
         """
         try:
             # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode('utf-8')
-            addr = client_sock.getpeername()
+            msg = self._current_client_socket.recv(1024).rstrip().decode('utf-8')
+            addr = self._current_client_socket.getpeername()
             logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
             # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode('utf-8'))
+            self._current_client_socket.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
-            client_sock.close()
+            if self._current_client_socket:
+                self._current_client_socket.close()
+            self._current_client_socket = None
 
     def __accept_new_connection(self):
         """
